ab5201970e4dd03dd5d3dbf2fdf69cab059e43b6
avlapeto <avlapeto@gmail.com>
2017-05-10 12:49:03 +0300

    Some little changes

--- js/index.js
+++ js/index.js
@@ -1,118 +1,126 @@
-class Circle{
-    constructor(radius, centerX, centerY, color, canvas){
-        this.radius = radius;
-        this.centerX = centerX;
-        this.centerY = centerY;
-        this.color = color;
-        this.canvas = canvas;
-        this.context = this.canvas.getContext();
-    }
-    draw(){
-
-        this.context.beginPath();
-        this.context.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI, false);
-        this.context.fillStyle = this.color;
-        this.context.fill();
-        this.context.closePath();
-
-    };
-
-    getCenter(){
-        return {
-            x: this.centerX,
-            y: this.centerY,
+let module = (()=>{
+    class Circle{
+        constructor(radius, centerX, centerY, color, canvas){
+            this.radius = radius;
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.color = color;
+            this.canvas = canvas;
+            this.context = this.canvas.getContext();
         }
+        draw(){
+
+            this.context.beginPath();
+            this.context.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI, false);
+            this.context.fillStyle = this.color;
+            this.context.fill();
+            this.context.closePath();
+
+        };
+
+        getCenter(){
+            return {
+                x: this.centerX,
+                y: this.centerY,
+            }
+        };
+
     };
 
-};
+    class  littleCircle extends Circle{
+        constructor(radius, centerX, centerY, color, canvas, border){
+            super(radius, centerX, centerY, color, canvas);
+            this.border = border;
+        };
 
-class  littleCircle extends Circle{
-    constructor(radius, centerX, centerY, color, canvas, border){
-        super(radius, centerX, centerY, color, canvas);
-        this.border = border;
+        move(centerX = 0, centerY = 0){
+            this.centerX += centerX;
+            this.centerY += centerY;
+        };
     };
 
-    move(centerX = 0, centerY = 0){
-        this.centerX += centerX;
-        this.centerY += centerY;
+    class Canvas{
+        constructor(id){
+            this.canvas = document.getElementById(id);
+            this.context = this.canvas.getContext('2d');
+        }
+
+        clear(){
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        };
+        getContext(){
+            return this.context;
+        };
+        getElement(){
+            return this.canvas;
+        };
+        getWidth(){
+            return this.canvas.width;
+        };
+        getHeight(){
+            return this.canvas.height;
+        };
     };
 
+    class MouseMoveListener{
+        constructor(bigCircle, littleCircle, canvas){
+            this.bigCircle = bigCircle;
+            this.littleCircle = littleCircle;
+            this.canvas = canvas;
+        }
 
-};
+        handle(event){
+            let rect = this.canvas.getElement().getBoundingClientRect(),
 
-class Canvas{
-    constructor(id){
-        this.canvas = document.getElementById(id);
-        this.context = this.canvas.getContext('2d');
+                //The cursor position relative to the center of the inner circle
+                relPosX = (event.clientX - rect.left) - this.littleCircle.getCenter().x,
+                relPosY = (event.clientY - rect.top) - this.littleCircle.getCenter().y,
+
+                //Square hypotenuse displacement
+                delta_2 = (this.littleCircle.radius + this.littleCircle.border)**2 - (relPosX**2 + relPosY**2);
+
+
+            // No need to move the inner circle
+            if(delta_2 <= 0) return;
+
+            let diffX = -Math.sqrt(delta_2)*Math.sign(relPosX)*Math.cos(Math.atan(Math.abs(relPosY/relPosX))),
+                diffY = -Math.sqrt(delta_2)*Math.sign(relPosY)*Math.cos(Math.atan(Math.abs(relPosX/relPosY))),
+                littleCircleInBigCircle = (this.littleCircle.getCenter().x + diffX - this.bigCircle.getCenter().x)**2 + (this.littleCircle.getCenter().y + diffY - this.bigCircle.getCenter().y)**2 <= (this.bigCircle.radius- this.littleCircle.radius)**2;
+
+            if(!littleCircleInBigCircle) return;
+
+            canvas.clear();
+            bigCircle.draw();
+            ltlCircle.move(diffX, diffY);
+            ltlCircle.draw();
+        }
     }
 
-    clear(){
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    let border = 10,
+        radiusBig = 200,
+        radiusSmall = 20,
+        canvas = new Canvas('myCanvas'),
+        centerX = canvas.getWidth() / 2,
+        centerY = canvas.getHeight() / 2,
+        bigCircle = new Circle(radiusBig, centerX, centerY, 'red', canvas),
+        ltlCircle = new littleCircle(radiusSmall, centerX + 50, centerY + 50, 'black', canvas, border),
+        mouseMoveListener = new MouseMoveListener(bigCircle, ltlCircle, canvas);
 
-    };
-    getContext(){
-        return this.context;
-    };
-    getElement(){
-        return this.canvas;
-    };
-    getWidth(){
-        return this.canvas.width;
-    };
-    getHeight(){
-        return this.canvas.height;
-    };
-};
+    canvas.clear();
+    bigCircle.draw();
+    ltlCircle.draw();
 
-class MouseMoveListener{
-    constructor(bigCircle, littleCircle, canvas){
-        this.bigCircle = bigCircle;
-        this.littleCircle = littleCircle;
-        this.canvas = canvas;
+    canvas.getElement().addEventListener('mousemove', (evt)=> {
+        mouseMoveListener.handle(evt);
+    }, false);
+
+    return {
+        canvas: canvas,
+        ltlCircle: ltlCircle,
+        bigCircle: bigCircle,
+        centerX: centerX,
+        centerY: centerY,
     }
+})();
 
-    handle(event){
-        let rect = this.canvas.getElement().getBoundingClientRect(),
-
-        //Положение курсора относительно центра внутренней окружности
-        relPosX = (event.clientX - rect.left) - this.littleCircle.getCenter().x,
-        relPosY = (event.clientY - rect.top) - this.littleCircle.getCenter().y,
-
-        //Квадрат гипотенузы перемещения
-        delta_2 = (this.littleCircle.radius + this.littleCircle.border)**2 - (relPosX**2 + relPosY**2);
-
-        if(delta_2 <= 0) return; /*Нет необходимости перемещать внутреннюю окружность*/
-
-        let diffX = -Math.sqrt(delta_2)*Math.sign(relPosX)*Math.cos(Math.atan(Math.abs(relPosY/relPosX))),
-        diffY = -Math.sqrt(delta_2)*Math.sign(relPosY)*Math.cos(Math.atan(Math.abs(relPosX/relPosY))),
-        littleCircleInBigCircle = (this.littleCircle.getCenter().x + diffX - this.bigCircle.getCenter().x)**2 + (this.littleCircle.getCenter().y + diffY - this.bigCircle.getCenter().y)**2 <= (this.bigCircle.radius- this.littleCircle.radius)**2;
-
-        if(!littleCircleInBigCircle) return;
-
-        canvas.clear();
-        bigCircle.draw();
-        ltlCircle.move(diffX, diffY);
-        ltlCircle.draw();
-    }
-}
-
-let border = 10,
-    radiusBig = 200,
-    radiusSmall = 20,
-    canvas = new Canvas('myCanvas'),
-    centerX = canvas.getWidth() / 2,
-    centerY = canvas.getHeight() / 2,
-    bigCircle = new Circle(radiusBig, centerX, centerY, 'red', canvas),
-    ltlCircle = new littleCircle(radiusSmall, centerX + 50, centerY + 50, 'black', canvas, border),
-    mouseMoveListener = new MouseMoveListener(bigCircle, ltlCircle, canvas);
-
-
-canvas.clear();
-bigCircle.draw();
-ltlCircle.draw();
-
-
-
-canvas.getElement().addEventListener('mousemove', (evt)=> {
-    mouseMoveListener.handle(evt);
-}, false);
--- js/test.js
+++ js/test.js
@@ -1,77 +1,79 @@
-mocha.setup('bdd');
-let assert = chai.assert;
+((module)=>{
+    mocha.setup('bdd');
+    let assert = chai.assert;
 
 
-describe("canvas", function() {
+    describe("canvas", function() {
 
-    describe("Ширина элемента", function() {
+        describe("Width of element", function() {
 
-        it("Возвращает значение типа int", function() {
-            assert(Number.isInteger(canvas.getWidth()), "Не int");
+            it("Return type int", function() {
+                assert(Number.isInteger(module.canvas.getWidth()), "Не int");
+            });
+
+            it("Width grater then null", function() {
+                assert.strictEqual(module.canvas.getWidth() > 0, true, 'Negative width');
+            });
+
+            it("Width grater then 5px", function() {
+                assert.isAbove(module.canvas.getWidth(), 5, 'Width lover then 5px');
+            });
+
+            it("Width grater then 1000px", function() {
+                assert.isAbove(module.canvas.getWidth(), 1000, 'Width lover then 1000px');
+            });
+
         });
 
-        it("Ширина больше нуля", function() {
-            // strictEqual исключительно в качестве примера
-            assert.strictEqual(canvas.getWidth() > 0, true, 'Ширина меньше нуля');
-        });
+        describe("Context", function() {
 
-        it("Ширина больше 5px", function() {
-            assert.isAbove(canvas.getWidth(), 5, 'Ширина меньше 5px');
-        });
+            it("Есть контекст", function() {
+                assert.isOk(module.canvas.getContext(), 'Empty object');
+            });
 
-        it("Ширина больше 1000px", function() {
-            assert.isAbove(canvas.getWidth(), 1000, 'Ширина меньше 1000px');
-        });
-
-    });
-
-    describe("Context", function() {
-
-        it("Есть контекст", function() {
-            assert.isOk(canvas.getContext(), 'Пустой объект');
-        });
-
-        it("Проверка класса", function() {
-            assert.instanceOf(canvas.getContext(), CanvasRenderingContext2D, 'Не является экземпляром CanvasRenderingContext2D');
-        });
+            it("Checking class", function() {
+                assert.instanceOf(module.canvas.getContext(), CanvasRenderingContext2D, 'Not instance of CanvasRenderingContext2D');
+            });
 
 
 
-    });
-
-});
-
-describe("Circle", function() {
-
-    describe("Центр окружности", function() {
-
-        it("Наличие координаты X", function() {
-            assert.property(ltlCircle.getCenter(), 'x', 'Отсутствует коордната X');
-        });
-
-        it(`Начальная координата X = ${centerX + 50}px`, function() {
-            assert.propertyVal(ltlCircle.getCenter(), 'x', centerX + 50, 'Не соответствует начальным условиям');
         });
 
     });
 
-    describe("Движение внутренней окружности", function() {
+    describe("Circle", function() {
 
-        it("Передвижение на расстояние x = 20", function() {
-            let dX = 20,
-                initial = ltlCircle.getCenter().x;
+        describe("Center of circle", function() {
 
-            canvas.clear();
-            bigCircle.draw();
-            ltlCircle.move(dX);
-            ltlCircle.draw();
+            it("Presence of a coordinate X", function() {
+                assert.property(module.ltlCircle.getCenter(), 'x', 'Missing coordinate X');
+            });
 
-            assert.propertyVal(ltlCircle.getCenter(), 'x', initial + dX, 'Неверное перемещение');
+            it(`Initial coordinate X = ${module.centerX + 50}px`, function() {
+                assert.propertyVal(module.ltlCircle.getCenter(), 'x', module.centerX + 50, 'Not equal to initial value');
+            });
+
         });
 
+        describe("Moving of inner circle", function() {
+
+            it("Moving to a distance x = 20", function() {
+                let dX = 20,
+                    initial = module.ltlCircle.getCenter().x;
+
+                module.canvas.clear();
+                module.bigCircle.draw();
+                module.ltlCircle.move(dX);
+                module.ltlCircle.draw();
+
+                assert.propertyVal(module.ltlCircle.getCenter(), 'x', initial + dX, 'Wrong moving');
+            });
+
+
+        });
 
     });
 
-});
+    mocha.run();
+})(module);
 
-mocha.run();
